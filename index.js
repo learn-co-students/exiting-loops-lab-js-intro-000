601e31920b452a06de5239912735718cc368353c
Tsi Lung <tsi.lung@gmail.com>
2016-08-07 21:10:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function breakOut(array, changeValue, stopValue) {
+  var i
+  for (i = 0; i<array.length; i++){
+    if (array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue;
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  var i
+  for (i = 0; i < array.length; i++){
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue;
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  var i;
+  for (i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null;
+}
