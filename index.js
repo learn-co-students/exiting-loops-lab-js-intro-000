320cbc961a8ae660c9ff0745082240793e4e8db5
Mitch Masterson <mitchell.masterson@gmail.com>
2016-08-18 02:52:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0, l = array.length; i < l; i++) {
 
-    if (array[i] == stopValue) {
+    if (array[i] === stopValue) {
       break
     }
 
