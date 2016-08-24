3818da7f3598aed492a2272024160698ba60b1b4
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-24 17:32:31 +0000

    Done.

--- index.js
+++ index.js
@@ -19,13 +19,17 @@ function keepGoing(array, changeValue, skipValue){
   }
   return array
 }
+function findFn(seed){
+  const c = Math.floor(Math.random(seed) * 100)
+  console.log(c)
+  return c
 
-/*function findFn(){
-  return value
-}*/
+}
 
-function findBy(array, findFn){
-  var testValue = findFn
+function findBy(array, callBack){
+  var testValue = callBack(5)
+  console.log(testValue)
+  console.log(array)
   for(var i=0,l=array.length;i<l;i++){
     if(array[i]==testValue){
       return array[i]
