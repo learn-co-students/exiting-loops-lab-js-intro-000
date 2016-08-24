c245c0710f5f2ace8be0bbc25d31633da7e5d513
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-24 03:13:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 function breakOut(array,changeValue,stopValue){
-  for(var i=0,l = array.length;i<l;i++){
-    if(array[i]=stopValue){
+  var i,l
+  for( i=0,l = array.length;i<l;i++){
+    if(array[i]==stopValue){
       break
     }
     array[i]=changeValue
   }
   return array
+}
+
+function keepGoing(array, changeValue, skipValue){
+  var i,l
+  for(i=0,l=array.length;i<l;i++){
+    if(array[i]==skipValue){
+      continue
+    }
+    array[i]=changeValue
+  }
+  return array
+}
+
+/*function findFn(){
+  return value
+}*/
+
+function findBy(array, findFn){
+  var testValue = findFn
+  for(var i=0,l=array.length;i<l;i++){
+    if(array[i]==testValue){
+      return array[i]
+    }
+  }
+  return null
 }
