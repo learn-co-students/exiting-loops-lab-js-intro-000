ec4ca09c02cbd03e4f256748aea7a00fed82c67e
Cindy Gao <cindygao@hotmail.com>
2016-08-28 21:16:57 +0000

    Done.

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array, findFn){
   for (var i=0; i<array.length; i++){
-    if (array[i] == findFn){
-      return findFn()
+    if (findFn(array[i])){
+      return array[i]
     }
   }
   return null
