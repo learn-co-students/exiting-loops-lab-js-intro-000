2bded205c5d3d71ec9ceb7c379c41e432d9da042
Villy Siu <villysiu@gmail.com>
2016-09-20 22:45:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function breakOut(array, changeValue, stopValue){
+  for (let i = 0, l = array.length; i < l; i++) {
+   if (array[i] === stopValue)
+      break
+    else
+      array[i] = changeValue
+
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, stopValue){
+  for (let i = 0, l = array.length; i < l; i++) {
+   if (array[i] === stopValue)
+    continue
+  else
+      array[i] = changeValue
+
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+   if (findFn(array[i]))
+    return array[i]
+  }
+
+      return null
+}
