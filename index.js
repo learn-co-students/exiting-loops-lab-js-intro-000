58aaf628e46692daa91af726e9140cbb8887027c
Damien de Lartigue <damien@delartigue.co.uk>
2016-10-18 15:10:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0; i < array.length; i++){
+    if(array[i] < stopValue){
+      array[i] = changeValue;
+    } else {
+      break;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for (var i = 0; i < array.length; i++){
+    if(array[i] === skipValue){
+      continue;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn){
+  for(var i = 0; i < array.length; i++){
+    if(findFn(array[i])){
+      return array[i];
+    } else {
+      continue;
+    };
+  };
+  return null;
+}
