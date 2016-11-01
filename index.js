bd786899fdd57610753b8a493c8f40b5cb0938e2
Ruby Mentor <rubymentor@gmail.com>
2016-11-01 09:08:51 +0000

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ function breakOut(array, changeValue, stopValue) {
       array[i] = changeValue;
     }
   }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i=0; i<array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  for (var i=0; i<array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null;
 }
