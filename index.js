2b2b5bf054b28aa0f2deeb5501c05db76557b39c
Amit Singh <amit_nyc@live.com>
2016-12-19 15:47:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue){
   for (let i = 0, l = array.length; i < l; i++) {
-  if (array === stopValue){
+  if (array === changeValue){
     break
   }
     array[i]=changeValue
