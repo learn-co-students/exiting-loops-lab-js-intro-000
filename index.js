6a665b65f73cf1216a39fc3c687718a901b49995
Siobhan Harley <siobhan.harley@gmail.com>
2016-12-28 18:05:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0, l = array.length; i <l; i++) {
+    if(array[i] === stopValue) {
+    break
+  }
+  else {
+    array[i] = changeValue;
+  }
+}
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(let i = 0, l = array.length; i < l; i++) {
+    if(array[i] === skipValue) {
+      continue;
+    }
+    else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+    for(let i = 0, l = array.length; i < l; i++) {
+      if(array[i] === findFN) {
+        var result = array[i];
+      }
+      else {
+        var result = null;
+      }
+    }
+    return result;
+}
+
+function findFn (array, value) {
+   for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] !== value) {
+      return null;
+      }
+     }
+   }
+
+
+function findBy(array, findFn) {
+    for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+}
+ 
