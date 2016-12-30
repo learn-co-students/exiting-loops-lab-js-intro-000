18635fd02f18d4045ec59410fcc67fb2e7db8744
Muhammad Mehdi <mohummad.mehdi@gmail.com>
2016-12-30 00:33:34 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,43 @@
 
 function breakOut(array, changeValue,stopValue){
   for (var i=0 ,l=array.length; i<l; i++){
-    if (array[i] >= stopValue){
+    
+    if (array[i] === stopValue){
       break;
     }
-    array[i]+changeValue;
+    else{
+      array[i] = changeValue;
+    }
   }
+  return array
 }
+ /* keepGoing(array, changeValue, skipValue) 
+ which iterates through array and changes
+every element to changeValue except those
+that match skipValue. Then return the array.
+*/
+function keepGoing(array,changeValue,skipValue){
+  for (var i= 0, l=array.length; i<l; i++){
+    if (array[i] === skipValue){
+      continue;
+    }
+    else{
+        array[i]= changeValue;
+    }
+  }
+  return array
+}
+/* findBy(array, findFn) which looks for a value
+ in array based on the return value of findFn. 
+ Return null if the value isn't found.
+ */
+
+ function findBy(array,findFn){
+   for( var i=0 , l= array.length; i<l; i++){
+     if (findFn(array[i])){
+       return array[i]
+     }
+     
+   }
+   return null; 
+ }
