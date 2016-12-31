85549811261ee35372600df5e96a64ac511639a2
Michael Lee <mleenavy@gmail.com>
2016-12-31 03:53:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var breakOut = (array, changeValue, stopValue) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+var keepGoing = (array, changeValue, skipValue) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+var findBy = (array, findFn) => {
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+    else null
+  }
+}
