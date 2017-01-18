2e1ff2d3f1d402789d51059326aa09a51917b5c5
Usman Pervaiz <de.usman@yahoo.com>
2017-01-18 03:49:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function breakOut(array, changeValue, stopValue) {
+  
+  for(var i = 0; i !== array.length; i++) {
+    if( array[i] === stopValue) {
+      break;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(var i = 0; i !== array.length; i++) {
+    if(array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  
+var result = null;
+array.forEach(function(element) { if (element === findFn) { result = findFn }})
+return result;
+
+} 
