da72e0a8742d7a93cca210dfb1118ca15075d182
Richard Burd <richardburdx@yahoo.com>
2017-01-19 20:22:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue
+    } else {
+      break
+    }
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue
+   }
+  }
+  return array
+ }
+
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] !== findFn) {
+      return null
+  }
+ }
+}
