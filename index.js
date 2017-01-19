f72beb578bddeda5fb1a2b347deb509eecc613d5
Richard Burd <richardburdx@yahoo.com>
2017-01-19 20:24:51 +0000

    Done.

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0, l = array.length; i < l; i++) {
-    if (array[i] !== findFn) {
-      return null
+    if (findFn(array[i])) {
+      return array[i]
   }
  }
+ return null
 }
