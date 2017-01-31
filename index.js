c1fa9e5c9838a21d5ca6fe902ef510e93947d04d
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-01-31 20:05:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break;
+    }else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  for (var i = 0; i < array.length; i++) {
+    var retValue = findFn(array[i]);
+    return array[i] === retValue ? array[i] : null;
+  }
+}
