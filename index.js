ff790a897d34b0eb07313aebcde48fb356e4b9c9
Tommy Mitchell <mitchell.tommy@gmail.com>
2017-04-07 20:58:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0, l = array.length; i < l ; i++){
+    if(array[i] != stopValue){
+      array[i] = changeValue
+    }
+    else{
+      break;
+    }
+  }
+return array
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for (var i = 0; i < array.length; i++){
+    if (array[i] ===  skipValue){
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function findBy(array, findFn){
+  for (var i = 0; i<array.length; i++){
+    if(findFn(array[i])){
+      return array[i]
+    } else {
+      continue
+    }
+  }
+  return null
+}
