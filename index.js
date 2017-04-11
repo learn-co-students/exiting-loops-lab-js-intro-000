12483331af4ace18acef27f8657185d4274ee560
Michal Vacek <vahyno@gmail.com>
2017-04-11 21:22:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0, l = array.length; i < l; i++) {
-  if (array[i] !== stopValue) {
+  if (array[i] == stopValue) {
     break
   }
-
-
-  
+  array[i] = changeValue
+ }
+ return array
 }
 
-//function keepGoing(array, changeValue, skipValue) {
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+  if (array[i] === skipValue) {
+    continue
+  }
+   array[i] = changeValue
+  }
+  return array
+}
 
-//}
-
-//function findBy(array, findFn){
-  
-//}
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+  if (findFn(array[i])) {
+  return array[i]
+  }
+  }
+  return null
+}
