b951b5bb9c0cf636776581b4037a421c34c1494d
Brian Balconi <bbalconi@gmail.com>
2017-05-03 04:05:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function breakOut (array, changeValue, stopValue) {
+  for (var i  = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue
+    }
+
+    else break
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i  = 0, l = array.length; i < l; i++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue
+    }
+
+    if (array[i] === skipValue) {
+      continue
+    }
+  }
+  return array
+}
+
+  function findBy(array, findFn) {
+    for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i]) == true) {
+        return array[i]
+    }
+  }
+}
