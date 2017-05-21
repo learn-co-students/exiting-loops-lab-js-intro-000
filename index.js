b93d70ef23fc7202bb0873dc2f30ecae0b2e71ca
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-05-21 01:21:00 +0000

    Done.

--- index.js
+++ index.js
@@ -21,10 +21,33 @@ function keepGoing(array, changeValue, skipValue){
   return array;
 }
 
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0, l = array.length; i < l; i++){
+    if (array[i] == stopValue){
+      break;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for (var i = 0, l = array.length; i < l; i++){
+    if (array[i] == skipValue){
+      continue;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
 function findBy(array, findFn){
-  for (var i = 0, i < array.length; i++){
-    if (findFn(array[i]))
+  for (var i = 0; i < array.length; i++){
+    if (array[i] === findFn){
       return array[i];
+    }
   }
   return null;
 }
