3d517d84fbdd098d73b0f33f7103f2dec2543c65
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-18 21:15:28 +0000

    Done.

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ function findBy(array, findFn) {
   for(var i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
       return array[i];
-    } else {
-      return null;
     }
   }
+  return null;
 }
