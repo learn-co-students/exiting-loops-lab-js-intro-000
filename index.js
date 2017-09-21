59d78409dc31790581162f588d2fae1efaa47319
Garry Clerge <clergeg@gmail.com>
2017-09-21 22:03:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function breakOut(array, changeValue, stopValue){
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === stopValue){
+      return array;
+
+    } else {array[i] = changeValue;}
+  }
+}
+
+
+function keepGoing(array, changeValue, skipValue){
+  for (let i = 0; i < array.length; i++){
+
+    if (array[i] === skipValue){
+      continue;
+    } else {array[i] = changeValue}
+  }
+
+  return array;
+
+}
+
+
+function findBy(array, findFn){
+
+ for (let i = 0; i < array.length; i++){
+   if (findFn(array[i])){
+     return array[i];
+   } else {continue}
+ }
+
+ return null;
+}
