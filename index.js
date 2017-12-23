3cb7bb0c9bf5bb56f3db7fba0a8b7ef6cc5164cb
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-23 17:54:17 +0000

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,23 @@ function breakOut(array, changeValue, stopValue){
     }
     array[i] = changeValue;
   }
+  return array;
+}
+function keepGoing(array, changeValue, skipValue){
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] !== skipValue)
+    array[i] = changeValue;
+  }
+  return array;
+}
+function findBy(array, findFn) {
+  for (var i = 0; i < array.length; i++) {
+    if(findFn(array[i])){
+      break;
+    }
+    else {
+      console.log(null);
+    }
+  }
+  return array[i];
 }
