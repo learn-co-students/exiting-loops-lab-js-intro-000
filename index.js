7ab22c0a401ed6fb693204028a2353e333eeea42
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-23 16:03:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break;
+    }
+    array[i] = changeValue;
+  }
+}
