cec148b51946aaac06634fa67c101867d9f70ea0
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-12 20:36:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function breakOut(array, changeValue, stopValue) {
+
+  for(var i =0; i <array.length; i++) {
+    if(array[i] === stopValue) {
+      break;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === skipValue) {
+      continue;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+
+  return array;
+}
+
+function findBy(array, findFn) {
+  var value = ''
+  for(var i = 0; i < array.length; i++) {
+    if(findFn(array[i])) {
+      value = array[i];
+    }
+  }
+
+if(!value) {
+  return null;
+} else {
+  return value;
+}
+
+
+}
