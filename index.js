bb455d00996d5ad11f4d84e57ad3ca8e633e8728
Mike C <mikecronin92@gmail.com>
2018-04-18 03:10:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i=0;i<array.length;i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else {
+      break;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i=0;i<array.length;i++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue;
+    } else {
+      continue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  for (let i=0;i<array.length;i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null;
+}
