5c8b1c57f20144f0320fdc04ad2968c38e51840a
Martin Cervantes <cervantes.martine@gmail.com>
2018-07-06 18:18:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function breakOut() {
+function breakOut(array, changeValue, stopValue) {
+    for(var i = 0; i < array.length; i++){
+        if(array[i] === stopValue){
+            break;
+        }
+        else {
+            array[i] = changeValue;
+        }
+    }
+    return array;
+}
 
+function keepGoing(array, changeValue, skipValue) {
+    for(var i = 0; i < array.length; i++){
+        if(array[i] === skipValue){
+            continue;
+        }
+        else {
+            array[i] = changeValue;
+        }
+    }
+    return array;
+}
+
+function findBy(array, findFn) {
+    for(var i = 0; i < array.length; i++)
+        if(array[i] === findFn)
+            return findFn;
+
+    return null;
 }
