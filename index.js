b711df13dff416da33fed4253b1822fd08eb351a
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-28 20:17:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) { break }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) { continue }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  for (var i = 0; i < array.length; i++) {
+    if (findFn(array[i])) { return array[i] }
+  }
+  return null
+}
