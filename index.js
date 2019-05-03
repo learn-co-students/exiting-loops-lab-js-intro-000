6aafff869f2af1938711708b909da2a242c4535f
Kailee Gray <kaileeagray@gmail.com>
2019-05-03 15:46:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// breakOut(array, changeValue, stopValue) which iterates through array and
-// changes every element to changeValue until the loop reaches stopValue.
-// Then we break out of the loop and return the array.
 function breakOut(array, changeValue, stopValue) {
   var j = array.length;
   for (var i = 0; i < j; i++) {
@@ -10,13 +7,24 @@ function breakOut(array, changeValue, stopValue) {
       array[i] = changeValue;
     }
   }
+  return array;
 }
 
-// keepGoing(array, changeValue, skipValue) which iterates through array and changes every element to changeValue except those that match skipValue. Then return the array.
-function keepGoing(array, changeValue, stopValue) {
-
+function keepGoing(array, changeValue, skipValue) {
+  var j = array.length;
+  for (var i = 0; i < j; i++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue;
+    }
+  }
+  return array;
 }
-// findBy(array, findFn) which looks for a value in array based on the return value of findFn. Return null if the value isn't found. Hint: Check the test file to know the signature of the findFn and the type of its return value
+
 function findBy(array, findFn) {
-
+  var j = array.length;
+  for (var i = 0; i < j; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
 }
