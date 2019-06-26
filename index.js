3a48b93366e349fc78e895c652f02c4915bf9d13
Hema Venkata <hvenkata@live.com>
2019-06-26 21:35:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      return array[i] = changeValue
+  } else {
+    break
+    return array
+  }
+}
