e4c65abfd8c27971ca7d8eb05b54094862735bc1
Hema Venkata <hvenkata@live.com>
2019-06-26 22:00:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] !== stopValue) {
-      return array[i] = changeValue
+      array[i] = changeValue
   } else {
     break
-    return array
   }
+}
+    return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue
+  } else {
+    continue
+  }
+}
+    return array
+}
+
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+
+  return null
 }
