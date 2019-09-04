377879d5386f428b7ce5f30bd4577f678599110e
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-04 05:43:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function breakOut( array, changeValue, stopValue) {
+	for (let i = 0; i < array.length; i++) {
+		array[i] = changeValue;
+		if (array[i + 1] === stopValue) {
+			break;
+		}
+	} return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === skipValue){
+			continue
+		} else {
+			array[i] = changeValue
+		}
+	} return array
+}
+
+function findBy (array, findFn) {
+	for (let i = 0; i < array.length; i++) {
+		if (findFn(array[i])) {
+			return array[i];
+		}
+	}return null
+}
