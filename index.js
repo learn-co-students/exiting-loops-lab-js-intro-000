50f0c23f6d8a459589f9e5b00be6f0210d35fa97
Avera Guthrie <averaguthrie@gmail.com>
2020-02-07 20:05:53 +0000

    Done.

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
-    if(!findFn(array[i])) {
-      return null;
+    if(findFn(array[i])) {
+      return array[i];
     }
   }
+  return null;
 }
