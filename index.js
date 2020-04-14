51ff2dd6455789abd3c8fb2b761dc104a0df148c
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-14 03:12:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  for(let i = 0; i < array.length; i++) {
+    if(findFn(array[i])) {
+      return array[i]
+    }
+  }
+  return null
+}
