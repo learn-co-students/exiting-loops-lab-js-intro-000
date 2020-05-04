8ed94e86398a14368f61d41b4eb110a2b2101f1b
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-04 07:58:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function breakOut(array, changeValue, stopValue){
+  for(var i=0; i<array.length; i++){
+    if(array[i] === stopValue){
+      break;
+    }else {array.splice(i, 1, changeValue);}
+}
+    return array;
+
+  }
+
+function keepGoing(array, changeValue, skipValue){
+  for(var i=0; i<array.length; i++){
+    if(array[i] === skipValue){continue;}
+    else{array[i] = changeValue;}
+  }
+  return array
+}
+
+function findBy(array, findFn){
+  for(var i=0; i<array.length; i++){
+    if(findFn(array[i])){
+      return array[i];
+    }
+  }
+}
