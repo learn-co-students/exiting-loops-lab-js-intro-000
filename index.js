6f0610987d99fec6c0bd827c940aa4205354b2e5
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-28 12:58:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function breakOut(array, changeValue, stopValue){
+for (let i = 0; i < array.length; i++) {
+  if (array[i] !== stopValue) {
+    array[i] = changeValue
+  }
+  else {
+    return(array)
+    }
+  }
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == skipValue){
+      continue
+    }
+    else{
+      array[i] = changeValue
+    }
+  }
+      return (array)
+}
+
+function findBy(array, findFn){
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])){
+      return (array[i])
+    }
+  }
+}
