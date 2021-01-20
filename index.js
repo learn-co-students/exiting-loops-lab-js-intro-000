11d7f9cf7aa91c23385e8e1239c46cf1f2e307fb
James Jensen <jnjensen@live.com>
2021-01-20 03:39:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == stopValue) {
+      break;}
+    else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == skipValue) {
+      continue;}
+    else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+
+function findBy(array, findFn) {
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null;
+}
