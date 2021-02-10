aefbe8118e3a8e8f7c9590c473470ef09abcc76e
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 21:26:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++ ) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else if (array[i] === stopValue){
+      break;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    } else if (array[i] !== skipValue) {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null;
+}
