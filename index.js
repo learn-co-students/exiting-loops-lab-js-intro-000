36c6333ac9177445576ca1b9591cc7466e407b0a
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-22 15:17:30 -0500

    Done.

--- index.js
+++ index.js
@@ -14,8 +14,15 @@ function keepGoing(array, changeValue, skipValue) {
  for (let i = 0; i < array.length; i++) {
           //array.push(array[i])
   if (array[i] !== skipValue) {
-  	 array[i] = changeValue
+  	continue
   }
+    array[i] = changeValue
  }
   return array 
 }
+
+function findBy(array, findFn) {
+	//match return value of function to value in array element (iterate over whole array) 
+}
+
+//expects return value of findBy to equal the search value 
